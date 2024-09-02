1e84cdecb01995cc55e677ba5fb3aac00ef4aa4a
Arsenis Chatziantoniou <arsenixatzi@hotmail.com>
2024-09-02 16:45:07 +0300

    ActiveMainButtonJs

--- js/script.js
+++ js/script.js
@@ -3,3 +3,12 @@ function toggleSearch(){
     const search = document.querySelector('.search')
     search.classList.toggle('active');
 }
+
+// 3d flipping
+
+function toggleFlip(){
+    const imgBox = document.querySelector('.imgBox');
+    const btn = document.querySelector('.btn');
+    imgBox.classList.toggle('active');
+    btn.classList.toggle('active');
+}
